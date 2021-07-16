9a797624fc738f640ecaf659ba03dfa0c196d35d
wnsrlf0721 <wnsrlf0721@ajou.ac.kr>
2021-07-16 22:14:42 +0900

    update name

--- air_monitor_server/air_monitor.js
+++ air_monitor_server/air_monitor.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT= 3001;
-const airmonitor_router = require('./router/index')
+const airmonitor_router = require('./router/router')
 const scheduler = require('./scheduler')
 
 app.use(express.json());
--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const config = require('./config/config');
 const client = require('./mqtt/control');
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}));
 
 //localhost:3000 페이지 실행
 app.get('/',function(req,res){
@@ -20,34 +20,34 @@ app.get('/',function(req,res){
             const parse = JSON.parse(message.toString());
             bulb[0]= parse[0];
             bulb[1]= parse[1];
-            console.log(bulb)
+            console.log(bulb);
         }
 
         else if(topic=='plug/sensor_status'){
             const parse = JSON.parse(message.toString());
             plug[0]= parse[0];
             plug[1]= parse[1];
-            console.log(plug)
+            console.log(plug);
         }
 
         else if(topic=='airmonitor/sensor_status'){
             const parse = JSON.parse(message.toString());
-            airmonitor = parse
-            console.log(airmonitor)
+            airmonitor = parse;
+            console.log(airmonitor);
         }
-    })
-})
+    });
+});
 
 //post method를 이용해 제어서버로 mqtt메시지 전송
 app.post('/bulb/command', function(req,res){
-    client.publish('bulb/update',JSON.stringify(req.body))
-    res.send(200)
-})
+    client.publish('bulb/update',JSON.stringify(req.body));
+    res.send(200);
+});
 app.post('/plug/command', function(req,res){
     client.publish('plug/update',JSON.stringify(req.body));
-    res.send(200)
-})
+    res.send(200);
+});
 
 app.listen(config.PORT, function(){
     console.log(`Server is up and running on port ${config.PORT}`);
-})
+});
